05c848d5ec65b50bdf701c3da4e8925c982216dc
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2025-06-29 17:08:50 -0300

    🔨  chore: Imodularizando o código

---
+++ src/spotify_endpoints/endpoints.py
@@ -0,0 +1,12 @@
+import requests as r
+import os
+import pprint
+
+def get_recently_played():
+    headers = {
+        "Authorization": f"Bearer {os.getenv('ACCESS_TOKEN')}"
+    }
+
+    response = r.get("https://api.spotify.com/v1/me/player/recently-played", headers=headers)
+
+    pprint.pprint(response.json().get("items"))
