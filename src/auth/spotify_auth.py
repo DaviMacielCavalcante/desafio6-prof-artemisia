bda916db2a3ec718bcede557fcc1fc21bbe2ebc2
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2025-06-29 18:22:30 -0300

    🔨  chore: Deixando o ódigo mais funcional

--- src/auth/spotify_auth.py
+++ src/auth/spotify_auth.py
@@ -6,21 +6,20 @@ import pprint
 import webbrowser
 from urllib.parse import urlparse, parse_qs
 import base64
-def get_authorization_code():
+def get_authorization_code(client_id: str, redirect_uri: str, state: str) -> None:
 
     load_dotenv(override=True)
-
-    client_id = os.getenv("CLIENT_ID")
-    redirect_uri = os.getenv("REDIRECT_URI")
 
     params = {
         "client_id": client_id,
         "response_type": "code",
         "redirect_uri": redirect_uri,
         "scope": "user-read-private user-read-email user-read-recently-played", 
-        "state": str(uuid.uuid4())
+        "state": state
     }
+
     response = r.get("https://accounts.spotify.com/authorize", params=params)
+
     webbrowser.open(response.url)
     callback_url = input("Digite a URL de callback: ")
     parsed_url = urlparse(callback_url)
@@ -30,20 +29,19 @@ def get_authorization_code():
     pprint.pprint(f"Authorization Code: {authorization_code[0] if authorization_code else 'Not found'}")
     set_key(".env", "AUTHORIZATION_CODE", authorization_code[0])
 
-def get_access_token():
+def get_access_token(redirect_uri: str, code: str, authorization_code: str) -> None:
     load_dotenv(override=True)
-    client_secret = os.getenv("CLIENT_SECRET")
-    client_id = os.getenv("CLIENT_ID")
     body = {
         "grant_type": "authorization_code",
-        "code": os.getenv("AUTHORIZATION_CODE"),
-        "redirect_uri": os.getenv("REDIRECT_URI")
+        "code": code,
+        "redirect_uri": redirect_uri
     }
 
     headers = {
         "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": f"Basic {base64.b64encode(f'{client_id}:{client_secret}'.encode()).decode()}"
+        "Authorization": authorization_code
     }
+
     response = r.post("https://accounts.spotify.com/api/token", data=body, headers=headers)
     pprint.pprint(response.json())
     access_token = response.json().get("access_token")
@@ -52,7 +50,7 @@ def get_access_token():
     set_key(".env", "REFRESH_TOKEN", refresh_token)
 
 
-def refresh_token():
+def refresh_token(refresh_token: str, authorization_code: str) -> None:
     load_dotenv(override=True)
     body = {
         "grant_type": "refresh_token",
@@ -61,7 +59,7 @@ def refresh_token():
 
     headers = {
         "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": f"Basic {base64.b64encode(f'{os.getenv("CLIENT_ID")}:{os.getenv("CLIENT_SECRET")}'.encode()).decode()}"
+        "Authorization": authorization_code
     }
 
     response = r.post("https://accounts.spotify.com/api/token", data=body, headers=headers)
@@ -71,8 +69,9 @@ def refresh_token():
     else:
         set_key(".env", "ACCESS_TOKEN", response.json().get("access_token"))
 
-def auth_flow():
+def auth_flow() -> None:
     set_key(".env", "AUTHORIZATION_CODE", "")
-    get_authorization_code()
-    get_access_token()
-    refresh_token()
+    get_authorization_code(os.getenv("CLIENT_ID"), os.getenv("REDIRECT_URI"), str(uuid.uuid4()))
+    load_dotenv(override=True)
+    get_access_token(os.getenv("REDIRECT_URI"), os.getenv("AUTHORIZATION_CODE"), f"Basic {base64.b64encode(f'{os.getenv('CLIENT_ID')}:{os.getenv('CLIENT_SECRET')}'.encode()).decode()}")
+    refresh_token(os.getenv("REFRESH_TOKEN"), f"Basic {base64.b64encode(f'{os.getenv('CLIENT_ID')}:{os.getenv('CLIENT_SECRET')}'.encode()).decode()}")
